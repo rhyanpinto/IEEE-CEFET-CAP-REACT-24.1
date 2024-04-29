b370f29d75128a97feda4a597e180fd778f44f81
Rhyan Pinto <rhyanpinto@gmail.com>
2024-04-29 17:07:09 -0300

    Parte 7

--- capacitacao/src/pages/Home/index.jsx
+++ capacitacao/src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Container } from './styles';
 
 export function Home() {
   const [isActive, setIsActive] = useState(false);
@@ -10,10 +11,10 @@ export function Home() {
   }
 
   return (
-    <>
+    <Container>
       {isActive ? <div>Está ativo</div> : <div>Não está ativo</div>}
       <button type='button' onClick={handleChangeIsActive}>{isActive ? "Desativar" : "Ativar"}</button>
       <button onClick={() => navigate('/about')}>About</button>
-    </>
+    </Container>
   )
 }
--- capacitacao/src/routes.jsx
+++ capacitacao/src/routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { Home } from './components/pages/Home';
-import { About } from './components/pages/About';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import GlobalStyle from "./styles/globalStyle"
 
 export const AppRoutes = () => {
   return (
@@ -10,6 +11,7 @@ export const AppRoutes = () => {
         <Route path='/about' element={<About />} />
         <Route path='*' element={<Navigate to='/home' />} />
       </Routes>
+      <GlobalStyle />
     </BrowserRouter>
   )
 }
