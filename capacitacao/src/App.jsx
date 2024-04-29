e12962a7568bae048e008a223a38dea7476257b6
Rhyan Pinto <rhyanpinto@gmail.com>
2024-04-29 15:59:56 -0300

    Parte 4

--- capacitacao/src/App.jsx
+++ capacitacao/src/App.jsx
@@ -1,28 +1,33 @@
 import { useEffect, useState } from 'react';
+import Button from './components/Button';
+
+// hooks são a forma de trabalhar com estado e ciclo de vida, sem trabalhar com
+// classes, somente com funções.
+
+// Effect hook -> permite executar efeitos colaterais em componentes funcionais.
+// São similares ao componentDidMount, componentDidUpdate, componentDidUnmount.
 
 function App() {
-  //Criamos um useState como vimos anteriormente
-  const [inputText, setInputText] = useState("");
-  
-  //Criamos uma função que sempre que receber um evento ela muda o valor de setInputText fazendo com que o valor seja salvo
-  function handleChange(event) {
-    setInputText(event.target.value);
-  }
-  
+  let [count, setCount] = useState(0);
+
   useEffect(() => {
-    console.log(inputText);
-  }, [inputText]);
+    alert("Iniciou!");
+  }, []);
+
+  useEffect(() => {
+    alert(`Clicou ${count} vezes`);
+  }, [count]);
+
+  function addCount() {
+    setCount(count + 1);
+  }
 
   return (
     <>
-      <p>{inputText}</p>
-      {/* Temos um input que recebe o que está em inputText e que executa a função onChange. Essa função é executada sempre que percebe qualquer tipo de mudança
-      E como ela recebe a função handleChange, sempre que ela perceber alguma mudança handleChange é executada e salva essa mudança no setInputText */}
-      <input type="text" value={inputText} onChange={handleChange} />  
-      <input type="text" onChange={handleChange} />  
-      <button type='button' onClick={() => alert(inputText)}>Clique aqui</button>
+      <h1>{count}</h1>
+      <Button text="Clique aqui" fx={() => addCount()}/>
     </>
   )
 }
 
-export default App
+export default App;
