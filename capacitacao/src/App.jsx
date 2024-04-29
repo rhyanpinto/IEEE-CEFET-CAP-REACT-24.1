ff7ba8c3771b4cc92ef2306c0acd3c30720e3047
Rhyan Pinto <rhyanpinto@gmail.com>
2024-04-29 15:50:47 -0300

    Parte 2

--- capacitacao/src/App.jsx
+++ capacitacao/src/App.jsx
@@ -1,22 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Button from './components/Button';
+
+// Ciclo de vida
+// Mounting: Sempre que renderizarmos um componente, o estado do componente tem que ser instanciado
+// unmounting: Quando o componente sair de tela, o estado do componente tem que ser limpado
+
+// Para trabalhar com ciclo de vida, precisamos usar classe ou hooks (nas funções).
+
+// normalmente, as alterações nos valores de variáveis modificados realizados em uma função
+// "desaparecem" quando sai da função que a modificou.
+
+// Estados são uma maneira de "preservar" valores entre as chamadas de funções.
+// useState é a forma de escrever estados sem usar classe.
 
 function App() {
+  let count = 0;
+  // let [count, setCount] = useState(0);
+
+  function addCount() {
+    count += 1;
+    alert(count);
+    // setCount(count + 1);
+  }
+
   return (
     <>
-      <h1>Hello World</h1>
-      <Button text="Clique aqui"/>
-      <Button>Click here!</Button>
-      <Button text="Clique ali"/>
-      <Button text="Clique acola"/>
-      <Button text="Clique"> aqui!</Button>
+      <h1>{count}</h1>
+      <Button text="Clique aqui" fx={() => addCount()}/>
     </>
-
-    // um componente não precisa ter obrigatoriamente todas as propriedades que
-    // foram atribuídas a ele, como é o caso do children.
-    )
+  )
 }
 
 export default App
